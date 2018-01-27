dd5654afd31df97882b95a5e236064f5319c511b
Rlht <R5TMASC@protonmail.com>
2018-01-27 13:07:49 +0100

    Landing 4
    Add main, tests.

--- landing/js/screenmove.js
+++ landing/js/screenmove.js
@@ -1,8 +1,5 @@
-var $arrow = $("#arrow");
-var $off = $("main").offset().top;
-
 $("#arrow").on("click", function () {
     $("body,html").animate({
-        scrollTop: $off
+        scrollTop: $("main").offset().top
     }, 1200)
 })
