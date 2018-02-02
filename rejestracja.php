e3957d85b78993f7015db3591da962731b5596e9
Rlht <R5TMASC@protonmail.com>
2018-02-02 13:54:14 +0100

    gra5
    walidacja cz.1

--- rejestracja.php
+++ rejestracja.php
@@ -1,3 +1,59 @@
+<?php 
+    session_start();
+    
+    if(isset($_POST['email'])) 
+    {
+        //odnosnik walidacji
+        $allgood = true;
+        //nick
+        $nick = $_POST['nick'];
+        if (strlen($nick)<3 || (strlen($nick)>20))
+        {
+           $allgood = false;
+           $_SESSION['e_nick'] = "Nick musi posiadac od 3 do 20 znakow";
+        }
+        if (ctype_alnum($nick)==false)
+        {
+            $allgood = false;
+            $_SESSION['e_nick'] = "Nick musi skladac sie z liter i cyfr, bez polskich znakow";
+        }
+        //email dla typu email to tezz string lel ale akceptuje np. aa@aa
+        $email = $_POST['email'];
+        $emailb = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if ((filter_var($emailb, FILTER_VALIDATE_EMAIL)==false) || ($emailb!=$email))
+        {
+            $allgood = false;
+            $_SESSION['e_email'] = "Email nieprawidlowy";
+            
+        }
+        //sprawdz poprawnosc hasla
+        $haslo1 = $_POST['haslo1'];
+        $haslo2 = $_POST['haslo2'];
+        if (strlen($haslo1) < 8 || (strlen($haslo1) > 20))
+        {
+            $allgood = false;
+            $_SESSION['e_haslo'] = "haslo musi posiadac od 8 do 20 znakow";
+        }
+        if ($haslo1 != $haslo2)
+        {
+            $allgood = false;
+            $_SESSION['e_haslo'] = "hasla sa niepoprawne";
+        }
+        
+        
+        
+        
+        if ($allgood)
+        {
+            //zapytanie insert
+            echo "udana walidacja";
+            exit();
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pl-PL">
 <head>
@@ -7,10 +63,31 @@
 </head>
 <body>
 	<form method="POST">
-		Nickname</br><input type="text" name="nick" /> </br>
+		Nickname</br><input type="text" name="nick" /> </br>		
+		<?php 
+		  if(isset($_SESSION['e_nick']))
+		  {
+		      echo '<span style="color:red">'.$_SESSION['e_nick'].'</span></br>';
+		      unset ($_SESSION['e_nick']);
+		  }
+		?>
 		e-mail</br><input type="email" name="email" /> </br>
+		<?php 
+		  if(isset($_SESSION['e_email']))
+		  {
+		      echo '<span style="color:red">'.$_SESSION['e_email'].'</span></br>';
+		      unset ($_SESSION['e_email']);
+		  }
+		?>
 		Haslo</br><input type="password" name="haslo1" /> </br>
-		Powtorz haslo</br><input type="text" name="haslo2" /> </br>
+		<?php 
+		  if(isset($_SESSION['e_haslo']))
+		  {
+		      echo '<span style="color:red">'.$_SESSION['e_haslo'].'</span></br>';
+		      unset ($_SESSION['e_haslo']);
+		  }
+		?>
+		Powtorz haslo</br><input type="password" name="haslo2" /> </br>
 		<label>
 			<input type="checkbox" name="regulamin"/> Akceptuje 
 		</label> 
